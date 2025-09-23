a54bc15e165165c4f109f8846ab5c53a90a372be
schismatiic <leonardojofre2003@gmail.com>
2025-09-22 22:13:31 -0300

    Elementos finales agregados
    Primera versión de este proyecto lista. Está abierta a futuras modificaciones.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch
+
+Este proyecto consiste en un Etch-a-Sketch o (como yo la conozco) pizarra mágica. Permite elegir el tamañó del grid y se adapta.
--- script.js
+++ script.js
@@ -1,17 +1,51 @@
+let valor = 0;
+const randomRGB = () => {
+  const random = Math.round(Math.random() * 100 * (64 / 25));
+  return random;
+};
+const adjustSize = (i) => {
+  const size = 500 / i;
+  return size;
+};
 const addGrid = (index = 16) => {
+  index = prompt("Grid size? \n Default: 16");
+  rowColumn.addEventListener("click", () => {
+    window.location.href = "index.html";
+  });
+  if (index > 100) {
+    index = 100;
+  } else if (index <= 0) {
+    index = 16;
+  }
   for (let i = 0; i < index; i++) {
     boxClass.style.cssText = "flex-direction: row";
     const rowDiv = document.createElement("div");
     rowDiv.style.cssText =
-      "display: flex; justify-content: center; align-items: center; flex-direction: row;";
+      "display: flex; justify-content: center; align-items: center; flex-direction: row; overflow: hidden;";
     boxClass.appendChild(rowDiv);
     for (let j = 0; j < index; j++) {
       const columnDiv = document.createElement("div");
       boxClass.style.cssText = "flex-direction: column";
-      columnDiv.style.cssText =
-        "flex-direction: row; padding: 10px 10px; border: .1px solid black";
+      columnDiv.style.cssText = `display: flex; width: ${adjustSize(
+        index
+      )}px; height: ${adjustSize(
+        index
+      )}px; border: .1px solid rgba(243, 11, 11, 0.1); flex-grow: 1`;
+
       columnDiv.style.background = "white";
+
       rowDiv.appendChild(columnDiv);
+      buttonRgb.addEventListener("click", () => {
+        columnDiv.addEventListener("mouseover", () => {
+          columnDiv.style.background = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`;
+        });
+      });
+      buttonDarkening.addEventListener("click", () => {
+        columnDiv.addEventListener("mouseover", () => {
+          valor = valor + 0.1;
+          columnDiv.style.opacity = `${valor}`;
+        });
+      });
       columnDiv.addEventListener("mouseover", () => {
         columnDiv.style.background = "black";
       });
@@ -21,9 +55,6 @@ const addGrid = (index = 16) => {
 const boxClass = document.getElementById("box");
 const tux = document.getElementById("tux");
 const rowColumn = document.getElementById("row__column");
-let grid = addGrid();
-
-rowColumn.addEventListener("click", () => {
-  const rc = prompt("???");
-  grid = addGrid(rc);
-});
+const buttonRgb = document.getElementById("rgb__button");
+const buttonDarkening = document.getElementById("darkening__button");
+addGrid();
